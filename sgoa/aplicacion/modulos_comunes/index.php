21c5ae488fb8c6752bdd73d78d8858a96783ec60
CaroCe <45445223+CaroCe@users.noreply.github.com>
2019-01-07 07:42:31 -0500

    Update index.php

--- sgoa/aplicacion/modulos_comunes/index.php
+++ sgoa/aplicacion/modulos_comunes/index.php
@@ -20,6 +20,7 @@ if (@!$_SESSION['usuario']) {
     <head>
         <meta charset="utf-8"></meta>
         <link rel="stylesheet" href="../../plugins/bootstrap/css/bootstrap.min.css"></link>
+        <link href="../../estilos/estilosHerramientas.css" rel="stylesheet" type="text/css"/>
         <script type="text/javascript" src="../../plugins/bootstrap/js/jquery-3.3.1.js"></script>
         <script type="text/javascript" src="../../plugins/bootstrap/js/bootstrap.min.js"></script>
         <script languaje = "javascript">
@@ -64,6 +65,8 @@ if (@!$_SESSION['usuario']) {
         body{
             margin:0;
             margin-bottom: 40px;
+            /*background-color: #FFFFCC;
+            background-image: url(../../images/fondotot.jpg);*/
         }
         /* Set black background color, white text and some padding */
         footer {
@@ -123,30 +126,47 @@ if (@!$_SESSION['usuario']) {
                 </div>
             </div>
         </nav>
-<table width="620px">
-	<tr>
-		<td width="20px"></td>
-		<td width="200px">Tittle</td>
-		<td width="200px">Date</td>
-		<td width="200px">Answers</td>
-	</tr>
-<?php 
-	include("conexionBD.php");
-	$query = "SELECT * FROM  foro WHERE identificador = 0 ORDER BY fecha DESC";
-	$result = $mysqli->query($query);
-	while($row = mysqli_fetch_array($result, MYSQLI_ASSOC)){
-		$id = $row['ID'];
-		$titulo = $row['titulo'];
-		$fecha = $row['fecha'];
-		$respuestas = $row['respuestas'];
-		echo "<tr>";
-			echo "<td><a href='foro.php?id=$id'>ver</a></td>";
-			echo "<td>$titulo</td>";
-			echo "<td>".date("d-m-y,$fecha")."</td>";
-			echo "<td>$respuestas</td>";
-		echo "</tr>";
-	}
-?>
-</table>
-<a href="formulario.php"> nuevo tema </a>
+        <!-- presentacion de objetos de aprendizaje-->
+        <div class="container text-center">
+            <div class="row content">
+                <!-- --------------------------------------------- -->
+                <div class="col-sm-12 text-center">
+                    <h2> <img src="../../images/foro.png"style="float:left;width:300px;height:170px">BIENVENIDO AL FORO DE AYUDA</h2>
+                    
+                    <div class="container-fluid" >
+                        <table  class="table table-striped"border ="1|1" class="table table-bordered" id="tabla">
+                            <thead style="background-color:#FFFFCC">
+                                <tr>
+                                    <td ></td>
+                                    <td>Titulo</td>
+                                    <td>Fecha</td>
+                                    <td>Respuestas</td>
+                                </tr>
+                            </thead>
+                    </div>
+                        <?php 
+                            include("conexionBD.php");
+                            $query = "SELECT * FROM  foro WHERE identificador = 0 ORDER BY fecha DESC";
+                            $result = $mysqli->query($query);
+                            while($row = mysqli_fetch_array($result, MYSQLI_ASSOC)){
+                                $id = $row['ID'];
+                                $titulo = $row['titulo'];
+                                $fecha = $row['fecha'];
+                                $respuestas = $row['respuestas'];
+                                echo "<tr style='background-color:#BAD6B8'>";
+                                    echo "<td><a href='foro.php?id=$id'><span class='glyphicon glyphicon-hand-right'> Participar</a></td>";
+                                    echo "<td>$titulo</td>";
+                                    echo "<td>".date("d-m-y,$fecha")."</td>";
+                                    echo "<td>$respuestas</td>";
+                                echo "</tr>";
+                            }
+                            echo '</table>';
+                        ?>
+                        <a type="button" class="btn btn-lg btn-primary" href="formulario.php"><i class='glyphicon glyphicon-plus'></i> nuevo tema </a>
+                </div>
 
+            </div>
+        </div>
+    </body>
+
+</html>
