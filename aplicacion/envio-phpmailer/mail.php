02befca81617ac43533e568d7672fdacd7f09f93
Miguel Clemente Aguilar Díaz <miguelclementeaguilardiaz@hotmail.com>
2019-08-06 08:28:59 -0500

    commit

--- aplicacion/envio-phpmailer/mail.php
+++ aplicacion/envio-phpmailer/mail.php
@@ -7,7 +7,7 @@ require 'PhpMailer/Exception.php';
 require 'PhpMailer/PHPMailer.php';
 require 'PhpMailer/SMTP.php';
 
-function enviarCorreo($correo,$usuario,$contraseña,$nombre,$apellido){
+function enviarCorreo($correo,$usuario,$contraseÃ±a,$nombre,$apellido){
 
     //note que este tipo de procedimiento se hace solo cuando se envia un correo desde el localhost
     //Instanciacion de las variables para el envio de correo
@@ -19,8 +19,8 @@ function enviarCorreo($correo,$usuario,$contraseña,$nombre,$apellido){
     $mail->isSMTP();                                            // Set mailer to use SMTP
     $mail->Host       = 'smtp.gmail.com';  // Specify main and backup SMTP servers  
     $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-    $mail->Username   = 'kristian192019d@gmail.com';                     // SMTP username
-    $mail->Password   = 'La bateriaesmejor';                               // SMTP password
+    $mail->Username   = 'aqui su correo';                     // SMTP username
+    $mail->Password   = 'Aqui su contasena';                               // SMTP password
     $mail->SMTPSecure = 'tls';                                  // Enable TLS encryption, `ssl` also accepted
     $mail->Port       = 587;                                    // TCP port to connect to                                  // TCP port to connect to
     $mail->SMTPOptions = array(
@@ -39,7 +39,7 @@ function enviarCorreo($correo,$usuario,$contraseña,$nombre,$apellido){
         // Content
         $mail->isHTML(true);                                  // Set email format to HTML
         $mail->Subject = 'Registro a la Plataforma exitoso'; 								//Asunto del mensaje
-        $mail->Body    = "<h2>El Sistema de Gestion de Objetos de Aprendizaje SGOA ".$nombre." ".$apellido."</h2> Bienvendido a la Plataforma Digital para el Intercambio de Recursos de Aprendizaje.<br>Tus credenciales son:<br>Usuario: ".$usuario."<br>Contraseña: ".$contraseña;
+        $mail->Body    = "<h2>El Sistema de Gestion de Objetos de Aprendizaje SGOA ".$nombre." ".$apellido."</h2> Bienvendido a la Plataforma Digital para el Intercambio de Recursos de Aprendizaje.<br>Tus credenciales son:<br>Usuario: ".$usuario."<br>ContraseÃ±a: ".$contraseÃ±a;
 
         $mail->send();
         echo 'Mensaje enviado con exito';
